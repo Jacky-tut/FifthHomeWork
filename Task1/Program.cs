74de76ca9957e496284ba07d44b7bfe5ccfc5c5c
Jacky_tut <netfinnov@mail.ru>
2022-12-20 00:01:14 +0300

    task one

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,4 +3,59 @@
 [345, 897, 568, 234] -> 2
 */
 
+int GetNumber(string message)
+{
+    int result;
 
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+int[] InitArray(int dimension)
+{
+    int[] array = new int [dimension];
+    Random rnd = new Random();
+    for (int i = 0; i < dimension; i++)
+    {
+        array[i] = rnd.Next(100, 999);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int EvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+        {
+            count++;
+        }
+        
+    }
+    return count;
+}
+
+int dimension = GetNumber("Введите размер массива");
+int[] array = InitArray(dimension);
+PrintArray(array);
+Console.WriteLine($"Количество четных чисел в массиве составляет {EvenNumbers(array)}");

4f635c287404490aff1f9c4f06d9db38bbdaef01
Jacky_tut <netfinnov@mail.ru>
2022-12-20 00:24:07 +0300

    task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,3 +4,59 @@
 [-4, -6, 89, 6] -> 0
 */
 
+int GetNumber(string message)
+{
+    int result;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+int[] InitArray(int dimension)
+{
+    int[] array = new int [dimension];
+    Random rnd = new Random();
+    for (int i = 0; i < dimension; i++)
+    {
+        array[i] = rnd.Next(-10, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int SumOddIndexNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+        
+    }
+    return sum;
+}
+
+int dimension = GetNumber("Введите размер массива");
+int[] array = InitArray(dimension);
+PrintArray(array);
+Console.WriteLine($"Сумма чисел с нечетными индексами равна {SumOddIndexNumbers(array)}");
